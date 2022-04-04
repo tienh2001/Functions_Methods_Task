895827367d94aa59c616e49cc26bdd8cf56fd7a0
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-04 15:47:34 +1000

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Greeting(string fname)
+	{
+		Console.WriteLine("Welcome Friends " + fname + "!");
+        Console.WriteLine("Have a Nice Day!");
+    }
+	public static void Main()
+	{
+		Greeting("John");
+	}
+}
