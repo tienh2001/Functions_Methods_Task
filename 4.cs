ac9bdcd0b9cb71b20c65c3139bbb057ae940b6d1
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 16:27:10 +1000

    Add files via upload

---
+++ 4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+					
+public class Program
+{
+	 static void Main(string[] args)
+	{
+		Console.WriteLine("Pleae input a string");
+		string input = Console.ReadLine();
+	int count = 0;
+	foreach (char item in input)
+	{
+		if (item == ' ')
+		{
+			count++; 
+		}
+	}
+		Console.WriteLine(input + " Contains " + count + " spaces");
+
+	}
+	
+}
