733a9e1cc43247847f9c7d1cff85ae04e5f1a6a3
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 15:27:35 +1000

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+					
+public class Calculator
+{
+	 static void Main(string[] args)
+	{
+		AddTwoNums(15, 16);
+	}
+	static void AddTwoNums(int num1, int num2) {
+		int ans = num1 + num2; 	
+			Console.WriteLine("The sum of two numbers is: " + ans);
+		
+	}
+}
