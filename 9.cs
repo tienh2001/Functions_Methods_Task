0653a702361d0f6eef6f75d647ae0c25c8593542
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 20:25:31 +1000

    Add files via upload

---
+++ 9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+					
+public class Program
+{
+	 static void Main(string[] args)
+	{
+		Console.WriteLine("Enter a number:");
+		int num = Convert.ToInt32(Console.ReadLine());
+		int k = 0;
+	for (int i = 1; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			k++;
+		}
+	}	
+	if (k == 2)
+	{
+		Console.WriteLine(" is a prime number");
+	}
+	else
+	{
+		Console.WriteLine(" is not a prime number");
+	}
+	}
+}
+
+
+
