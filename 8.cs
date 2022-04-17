fab0c92a05f7c8971cb47737569ee90c22f3bf8e
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 20:10:29 +1000

    Add files via upload

---
+++ 8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+					
+public class Program
+{
+	 static void Main(string[] args)
+	{
+		Console.WriteLine("Input number of Fibonacci series:");
+		int x = Convert.ToInt32(Console.ReadLine());
+
+		int next = 0;
+		int previous = 0;
+		for (int i = 0; i <= x; i++)
+		{
+			Console.WriteLine(next);
+			if (next == 0)
+			{
+				next = 1;
+			}
+			else 
+			{
+				int temp = next;
+				next = next + previous;
+				previous = temp;
+
+			}
+		}
+
+	}
+}
+
+
+
