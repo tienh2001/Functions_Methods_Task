b330f3f5153a03f8f3ba63d151d609bb197e05bb
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 20:46:34 +1000

    Add files via upload

---
+++ 10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+					
+public class Program
+{
+	 static void Main(string[] args)
+	{
+	int number, sum = 0;
+
+	Console.WriteLine("Enter a number:");
+	number = Convert.ToInt32(Console.ReadLine());
+
+	while (number > 0)
+	{
+		sum += (number % 10);
+		number /= 10;
+	}
+	Console.WriteLine("The sum of the number " + "is : " + sum);
+	}
+}
+
+
+
