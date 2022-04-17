38f13aa559c52791289dac71e7ee176fe2c8e2a7
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 16:28:21 +1000

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+					
+public class Program
+{
+	 static void Main(string[] args)
+	{
+	int[] numbers = new int[5];
+
+	numbers[0] = 5;
+	numbers[1] = 7;
+	numbers[2] = 3;
+	numbers[3] = 2;
+	numbers[4] = 9;
+	
+	int sum = 0;
+	foreach (var i in numbers)
+	{
+		sum += i;
+	}
+	Console.WriteLine("The sum of the elements of the array is " + sum);
+	}
+	
+}
