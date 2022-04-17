f860ff4b99688941c73c7032ed4aba13516be62f
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 19:54:48 +1000

    Add files via upload

---
+++ 7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+					
+public class Program
+{
+	 static void Main(string[] args)
+	{
+		int number, power;
+
+		Console.WriteLine("Input base number: ");
+		number = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Input the exponent: ");
+		power = Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("So, the number " + number + "^" + power + "=" + pow(number, power));
+	}
+	
+		static int pow(int number, int power)
+	{
+		if (power == 0)
+		return 1;
+		return number * pow(number, --power);
+	}
+}
+
+
+
