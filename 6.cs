163f3412625204348833b0d895731be3c0c44126
tienh2001 <100637096+tienh2001@users.noreply.github.com>
2022-04-17 16:43:10 +1000

    Add files via upload

---
+++ 6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+					
+public class Program
+{
+	 static void Main(string[] args)
+	{
+	int num1, num2, temp;
+	Console.WriteLine("Enter a number");
+	num1 = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine("Enter another number");
+	num2 = Convert.ToInt32(Console.ReadLine());
+
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
+
+	Console.WriteLine("Now the 1st number is: " + num1 + ", and the 2nd number is: " + num2);
+	}
+	
+}
